303fe814a90de65970fdec30b39952150891ca59
xuanlinhha <xuanlinhha@gmail.com>
2022-07-09 23:10:52 +0800

    Update

--- app/main.go
+++ app/main.go
@@ -12,9 +12,11 @@ import (
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/joho/godotenv/autoload"
+	"github.com/labstack/echo-contrib/prometheus"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/sirupsen/logrus"
+	"vantu.org/go-backend/model"
 	"vantu.org/go-backend/phrase/api"
 
 	"vantu.org/go-backend/phrase/repo"
@@ -27,18 +29,20 @@ func main() {
 	if strings.TrimSpace(port) == "" {
 		port = ":3000"
 	}
-	charsPath := os.Getenv("CHARS_PATH")
-	phrasesPath := os.Getenv("PHRASES_PATH")
-	sqliteDB := os.Getenv("SQLITE_DB")
-	timeout := os.Getenv("TIMEOUT")
-	timeoutValue, err := strconv.Atoi(timeout)
+	timeout, err := strconv.Atoi(os.Getenv("TIMEOUT"))
 	if err != nil {
-		logrus.Fatal("Invalid Timeout: ", timeout)
+		logrus.Fatal("Invalid Timeout: ", err)
 	}
-	logrus.Info("timeoutValue = ", timeoutValue)
+	config := model.Config{
+		Port:        port,
+		CharsPath:   os.Getenv("CHARS_PATH"),
+		PhrasesPath: os.Getenv("PHRASES_PATH"),
+		SqliteDB:    os.Getenv("SQLITE_DB"),
+		Timeout:     timeout,
+	}
 
-	// read static data
-	charsJsonFile, err := os.Open(charsPath)
+	// read chars path
+	charsJsonFile, err := os.Open(config.CharsPath)
 	if err != nil {
 		logrus.Fatal("Cannot Open Chars Json: ", err)
 	}
@@ -50,7 +54,8 @@ func main() {
 	var allChars []string
 	json.Unmarshal(byteValue, &allChars)
 
-	phrasesJsonFile, err := os.Open(phrasesPath)
+	// read phrases path
+	phrasesJsonFile, err := os.Open(config.PhrasesPath)
 	if err != nil {
 		logrus.Fatal("Cannot open Phrases: ", err)
 	}
@@ -62,23 +67,25 @@ func main() {
 	var allPhrases []string
 	json.Unmarshal(byteValue, &allPhrases)
 
-	// server
-	e := echo.New()
-	e.Use(middleware.CORS())
-
-	// DB
-	db, err := sqlx.Connect("sqlite3", sqliteDB)
+	// Initialization
+	db, err := sqlx.Connect("sqlite3", config.SqliteDB)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
-
-	// Initialization
 	phraseRepo := repo.NewPhraseRepo(db)
-	timeoutContext := time.Duration(timeoutValue) * time.Second
+	timeoutContext := time.Duration(config.Timeout) * time.Second
 	phraseService := service.NewPhraseService(phraseRepo, timeoutContext)
 	searchService := service.NewSearchService(allChars, allPhrases)
+
+	// server
+	e := echo.New()
+	e.Use(middleware.CORS())
 	api.NewPhraseHandler(e, phraseService, searchService)
+
+	// metrics
+	p := prometheus.NewPrometheus("echo", nil)
+	p.Use(e)
 
 	if err := e.Start(port); err != http.ErrServerClosed {
 		log.Fatal(err)
---
+++ model/config.go
@@ -0,0 +1,9 @@
+package model
+
+type Config struct {
+	Port        string
+	CharsPath   string
+	PhrasesPath string
+	SqliteDB    string
+	Timeout     int
+}
